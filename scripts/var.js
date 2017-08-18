dc9e5ae46002ba433922f81451873434bd6e784a
liujuanjuan1984 <qiaoanlu@163.com>
2017-08-18 23:10:38 +0800

    定义时间开销日志变量

---
+++ scripts/var.js
@@ -0,0 +1,29 @@
+/*
+*
+*定义金钱开销相关的变量。
+*
+*
+*/
+
+
+//定义：一条金钱开销日志
+
+var usedMoneyLog = {
+usedMoneyDate: usedMoneyDate,
+usedMoneyAccountFrom: usedMoneyAccountFrom,
+usedMoneyAccountTo: usedMoneyAccountTo,
+usedMoneyNum: usedMoneyNum,
+usedMoneyFor: usedMoneyFor,
+usedMoneyKind: usedMoneyKind,
+};
+
+usedMoneyDate = '2017-08-18';
+usedMoneyAccountFrom = '我的现金账户';
+usedMoneyAccountTo = '人家的钱包';
+usedMoneyNum = '-9.80';
+usedMoneyFor = '买一杯咖啡';
+usedMoneyKind = '个人生活消费';
+
+console.log(usedMoneyLog);
+
+
